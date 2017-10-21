4626d098a06658a717a0e9c4b620e0f18a8df51c
Christer Enfors <christer.enfors@gmail.com>
2017-10-21 13:45:55 +0200

    Add exception handling for KeyboardInterrupt
    Also print a message when example.py is started, saying that you can use
    Ctrl-c to exit.

--- example.py
+++ example.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-"Example file illustrating how to use twitgrep from another program."
+"""Example file illustrating how to use twitgrep from another program."""
 
 import twitgrep
 
-SEARCH_TERMS=["python", "#svpol", "emacs"]
+SEARCH_TERMS = ["python", "#svpol", "emacs"]
 
 print("Streaming search results for search terms: " + str(SEARCH_TERMS))
+print("Press Ctrl-c to exit.")
 
-for status in twitgrep.TwitGrep(SEARCH_TERMS):
-    print("----------------------------\nTweet from user '%s':\n%s" % \
-          (status.user.screen_name, status.text))
+try:
+    for status in twitgrep.TwitGrep(SEARCH_TERMS):
+        print("----------------------------\nTweet from user '%s':\n%s" %
+              (status.user.screen_name, status.text))
+except KeyboardInterrupt:
+    print()  # Get away from the "^C" left on the screen by pressing Ctrl-c
+    raise SystemExit
