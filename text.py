a10d937694ec4b6755bb769042019ead7c651c96
Christer Enfors <christer.enfors@gmail.com>
2017-10-23 15:01:38 +0200

    Add comparisons for Word and NGram

--- text.py
+++ text.py
@@ -32,6 +32,16 @@ class Word(object):
     >>> word4 = Word("@enfors")
     >>> word4
     Word('@enfors', word_type=3)
+
+    We can also do comparisons:
+
+    >>> word5 = Word("hello")
+    >>> word6 = Word("there")
+    >>> word7 = Word("hello")
+    >>> word5 == word6
+    False
+    >>> word5 == word7
+    True
     """
 
     TYPE_WORD = 1
@@ -39,18 +49,23 @@ class Word(object):
     TYPE_USERNAME = 3
     TYPE_TAG = 4
 
-    def __init__(self, word_text, word_type=None):
+    def __init__(self, word_text, word_type=None, keep_case=None):
 
         if word_type is None:
             if "://" in word_text:
                 word_type = Word.TYPE_URL
             elif word_text[0] == "@":
                 word_type = Word.TYPE_USERNAME
+                if keep_case is None:
+                    keep_case = True
             elif word_text[0] == "#":
                 word_type = Word.TYPE_TAG
             else:
                 word_type = Word.TYPE_WORD
 
+        if keep_case is not True:
+            word_text = word_text.lower()
+                
         self.word_text = word_text
         self.word_type = word_type
 
@@ -63,8 +78,11 @@ class Word(object):
     def __len__(self):
         return len(self.word_text)
 
-    def __getitem__(self, position):
-        pass
+    def __eq__(self, other_word):
+        if self.word_text == other_word.word_text:
+            return True
+        else:
+            return False
 
 
 class Sentence(list):
@@ -83,6 +101,28 @@ def normalize(text):
     """
 
     text = normalize_whitespace(text)
+    text = unify_sentence_dividers(text)
+    text = remove_junk_chars(text)
+
+    return text
+
+
+def unify_sentence_dividers(text):
+    """Return copy of text with ? and ! replaced with .
+    """
+
+    for ch in ["!", "?"]:
+        text = text.replace(ch, ".")
+
+    return text
+
+
+def remove_junk_chars(text):
+    """Return copy of text without unneeded chars.
+    """
+
+    for ch in [",", ":", ";"]:
+        text = text.replace(ch, "")
 
     return text
 
@@ -169,6 +209,19 @@ class NGram(object):
     some words
     >>> len(bi_gram)
     2
+
+    We can also do comparisons:
+
+    >>> word1 = Word("one")
+    >>> word2 = Word("two")
+    >>> word3 = Word("one")
+    >>> ngram1 = NGram([word1, word2])
+    >>> ngram2 = NGram([word3, word2])
+    >>> ngram1 == ngram2
+    True
+    >>> ngram3 = NGram([word1, word2, word3])
+    >>> ngram1 == ngram3
+    False
     """
 
     def __init__(self, words):
@@ -191,9 +244,19 @@ class NGram(object):
 
         return output.strip()
 
+    def __eq__(self, other_ngram):
+        if len(self) != len(other_ngram):
+            return False
+
+        for i in range(0, len(self.words)):
+            if self.words[i] != other_ngram.words[i]:
+                return False
+
+        return True
+
 
 def make_ngrams(words, n):
-    """Make n-grams from a list of words.
+    """Return n-grams from a list of Words.
     """
 
     num_words = len(words)
@@ -228,34 +291,35 @@ That is not dead which can eternal lie,
 and with strange aeons, even death may die.
 """
     sentences = normalize_and_split_sentences(text)
+    sentence = sentences[5]
 
     for sentence in sentences:
         print("%s." % "\n".join(textwrap.wrap("  " + sentence)))
 
-    print("sentences[0]:", sentences[0])
+    print("sentence:", sentence)
 
     print("\nbi-grams:")
 
-    n_grams = make_ngrams(split_sentence(sentences[0]), 2)
+    n_grams = make_ngrams(split_sentence(sentence), 2)
 
     for n_gram in n_grams:
         print(str(n_gram))
 
-    # print("\n3-grams:")
+    print("\n3-grams:")
 
-    # ngrams = make_ngrams(sentences[0], 3)
+    ngrams = make_ngrams(split_sentence(sentence), 3)
 
-    # for ngram in ngrams:
-    #     print(ngram)
+    for ngram in ngrams:
+        print(ngram)
 
     # print("\n4-grams:")
 
-    # ngrams = make_ngrams(sentences[0], 4)
+    # ngrams = make_ngrams(split_sentence(sentence), 4)
 
     # for ngram in ngrams:
     #     print(ngram)
 
 
 if __name__ == "__main__":
-    demo()
+#    demo()
     doctest.testmod()
